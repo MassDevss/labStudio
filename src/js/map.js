ea8a50a53c41b0aeedd037f01b564e8e3283e831
Manuel sSaNt0 <samaniegoserranom@gmail.com>
2024-04-01 21:51:51 -0700

    astro version

--- src/js/map.js
+++ src/js/map.js
@@ -1,12 +1,20 @@
 
+import { Loader } from "@googlemaps/js-api-loader"
+
+const loader = new Loader({
+  apiKey: "AIzaSyAjLuO9HRAUgr7ryKTKYZspmnvK8mRtwdM",
+  version: "weekly",
+});
+
+
 // Initialize and add the map
 let map;
-async function initMap() {
+
+loader.load().then(async () => {
 
   const position = { lat: 25.8055853, lng: -108.9964254 };
 
   const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
 
   // The map, centered at LabStudio
   map = new Map(document.getElementById("map"), {
@@ -15,20 +23,21 @@ async function initMap() {
     mapId: "ebec91dda5c2b1c2",
   });
 
-  const styleMarker = new google.maps.marker.PinView({
-    background: "#633fb9",
-    borderColor: "#5f3cb1",
-    glyphColor: "#5636a1",
-  });
-
   // The marker, positioned at LabStudio
-  const marker = new AdvancedMarkerView({
+  const marker = new google.maps.Marker({
     map: map,
     position: position,
     title: "LabStudio Office",
-    content: styleMarker.element
   });
-}
 
-initMap();
+  marker.setIcon({
+    path: google.maps.SymbolPath.CIRCLE,
+    scale: 8,
+    fillColor: "#633fb9",
+    fillOpacity: 0.8,
+    strokeColor: "#5f3cb1",
+    strokeWeight: 2
+  });
+
+});
 
