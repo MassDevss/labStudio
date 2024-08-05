9d909752b37fb07791592925a9ab790a30a4dfdd
Manuel Samaniego Serrano <ssant0@Manuels-MacBook-Air.local>
2024-08-04 22:13:30 -0700

    google analytics implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"]
+      }
+    })
+  ]
+});
