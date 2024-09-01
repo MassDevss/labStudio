bc1893cb6727fd4a8cb71ba81b26db21c26e6380
Manuel Samaniego <samaniegoserrom@gmail.com>
2024-08-31 21:57:21 -0700

    blog entry modified; change jobTitle property to skill is more rasonable.

---
+++ src/enums/JobSkill.ts
@@ -0,0 +1,14 @@
+
+
+export enum JobSkills {
+  Droner = 'Droner',
+  WebDeveloper = 'Desarrollador Web',
+  Photographer = 'Fotógrafo',
+  Designer = 'Diseñador',
+  VideoEditor = 'Editor de Video',
+  SocialMediaManager = 'Community Manager',
+  Copywriter = 'Copywriter',
+  Marketing = 'Marketing',
+  Sales = 'Ventas',
+}
+
